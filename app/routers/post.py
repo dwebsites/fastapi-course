5bb9e126bdd96bcb234550f4ef0f4e0d2c010d3e
dwebsites <89631078+dwebsites@users.noreply.github.com>
2021-11-15 20:56:23 +0100

    refactor: Use prefix in paths

--- app/routers/post.py
+++ app/routers/post.py
@@ -4,11 +4,13 @@ from typing import  List
 from .. import models, schemas
 from ..database import engine, get_db
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/posts"
+)
 
 
 
-@router.get("/posts", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db)):
     #cursor.execute("""SELECT * FROM posts """)
     #posts = cursor.fetchall()
@@ -16,7 +18,7 @@ def get_posts(db: Session = Depends(get_db)):
     return  posts
 
 
-@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES 
     # (%s, %s, %s) RETURNING * """,
@@ -32,7 +34,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return  new_post
 
 
-@router.get("/posts/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
 
     # cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(id),))
@@ -45,7 +47,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
                            detail=f"post with id: {id} was not found")
     return post
 
-@router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db)):
     
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
@@ -61,7 +63,7 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@router.put("/posts/{id}", response_model=schemas.Post)
+@router.put("/{id}", response_model=schemas.Post)
 def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
