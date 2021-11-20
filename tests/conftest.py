560250a822886a0d81f50bbb77220907f0315db1
dwebsites <dondim@protonmail.com>
2021-11-20 15:23:43 +0100

    add more tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -61,6 +61,17 @@ def test_user(client):
     new_user['password'] = user_data["password"]
     return new_user
 
+@pytest.fixture()
+def test_user2(client):
+    user_data = {"email": "test2@gmail.com",
+                 "password": "123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['email'] = user_data["email"]
+    new_user['password'] = user_data["password"]
+    return new_user
 
 @pytest.fixture
 def token(test_user):
@@ -77,7 +88,7 @@ def authorized_client(client, token):
 
 
 @pytest.fixture
-def test_posts(test_user, session):
+def test_posts(test_user, session, test_user2):
     posts_data = [{
         "title": "first title",
         "content": "first content",
@@ -92,9 +103,9 @@ def test_posts(test_user, session):
         "content": "3rd content",
         "owner_id": test_user['id']
     }, {
-        "title": "3rd title",
-        "content": "3rd content",
-        "owner_id": test_user['id']
+        "title": "4rd title",
+        "content": "4rd content",
+        "owner_id": test_user2['id']
     }]
 
     def create_post_model(post):
